65597a43b5c75c29f8908f9c0d803b3096c4b868
stephenbatt <35442763+stephenbatt@users.noreply.github.com>
2026-03-06 03:06:41 -0600

    Fix typo in uvicorn.run parameters

--- backend/server.py
+++ backend/server.py
@@ -893,4 +893,4 @@ if __name__ == "__main__":
     import os
 
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)t=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
